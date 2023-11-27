1e3aa41e4725c7c91bea1bcedaad0b7e21426adb
ciuzaak <as460230379@hotmail.com>
2023-11-28 01:54:17 +0800

    customize

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd.colorscheme("catppuccin")
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
